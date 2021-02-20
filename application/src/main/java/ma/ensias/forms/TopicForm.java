5095421352062f1b2574c4b18dc089bfbf38a26a
fimtow <62258310+fimtow@users.noreply.github.com>
2021-02-20 12:30:23 +0100

    added topic page api

---
+++ application/src/main/java/ma/ensias/forms/TopicForm.java
@@ -0,0 +1,56 @@
+package ma.ensias.forms;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import ma.ensias.beans.Post;
+import ma.ensias.beans.Topic;
+import ma.ensias.dao.DAOFactory;
+import ma.ensias.dao.PostDao;
+import ma.ensias.dao.TopicDao;
+
+public class TopicForm {
+    private static final String ID_FIELD  = "id";
+
+    private boolean success = true;
+    private List<Post> posts;
+    public boolean getResult() {
+        return success;
+    }
+
+
+    public Topic searchTopic( HttpServletRequest request ) {
+
+        int id = Integer.parseInt(getFieldValue( request, ID_FIELD ));
+        
+        DAOFactory daoFactory = DAOFactory.getInstance();
+        TopicDao topicDao = daoFactory.getTopicDao();
+        
+        Topic topic = topicDao.find(id);
+        
+        if(topic == null)
+        	success = false;
+        
+        if(success)
+        {
+        	PostDao postDao = daoFactory.getPostDao();
+        	posts = postDao.find(topic);
+        }
+        return topic;
+    }
+    
+    public List<Post> getPosts()
+    {
+    	return posts;
+    }
+
+    private static String getFieldValue( HttpServletRequest request, String fieldName ) {
+        String value = request.getParameter( fieldName );
+        if ( value == null || value.trim().length() == 0 ) {
+            return null;
+        } else {
+            return value;
+        }
+    }
+}
