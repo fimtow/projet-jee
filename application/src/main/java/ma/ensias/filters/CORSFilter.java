ddd9f9995e3c896e7336219d0f3fc7f132c0f9cb
fimtow <62258310+fimtow@users.noreply.github.com>
2021-02-23 12:31:42 +0100

    added CORS filter

---
+++ application/src/main/java/ma/ensias/filters/CORSFilter.java
@@ -0,0 +1,43 @@
+package ma.ensias.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CORSFilter implements Filter {
+	 
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+            FilterChain filterChain) throws IOException, ServletException {
+         
+ 
+        HttpServletRequest  request   = (HttpServletRequest)servletRequest;
+        HttpServletResponse response  = (HttpServletResponse)servletResponse;
+ 
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS");
+        response.addHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"));     
+  
+        // For HTTP OPTIONS verb reply with ACCEPTED status code, for CORS handshake
+        if (request.getMethod().equals("OPTIONS")) {
+            response.setStatus(HttpServletResponse.SC_ACCEPTED);
+            return;
+        }
+ 
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+ 
+    public void destroy() {
+        // TODO Auto-generated method stub
+    }
+ 
+    public void init(FilterConfig arg0) throws ServletException {
+        // TODO Auto-generated method stub       
+    }   
+}
