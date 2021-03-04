5e3516b8d24fa4c74d9d194a50491a75c466e61c
AJOthmane <aja.oth@gmail.com>
2021-03-04 17:43:28 +0100

    Deleted some useless lines of code

--- application/src/main/java/ma/ensias/servlets/PostServlet.java
+++ application/src/main/java/ma/ensias/servlets/PostServlet.java
@@ -7,13 +7,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 import ma.ensias.beans.Post;
-import ma.ensias.dao.DAOFactory;
 import ma.ensias.forms.PostForm;
 
 
@@ -62,9 +60,6 @@ public class PostServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		HttpSession session =  request.getSession();
-		session.setAttribute("userSession",DAOFactory.getInstance().getUserDao().find(1));
 		
 		PostForm postForm = new PostForm();
 		postForm.createPost(request);
